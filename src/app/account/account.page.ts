cedb28dc528b577aa925b5d148cbd2f660ee64a3
PinsardF <mysteries@orange.fr>
2020-11-18 18:07:17 +0100

    account page done (no picture yet)

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators  } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export interface Account {
   firstName: string;
@@ -15,16 +15,30 @@ export interface Account {
 export class AccountPage implements OnInit {
 
   account: Account;
-  emailForm = new FormControl('', [Validators.required, Validators.email]);
+  formGroup = new FormGroup({
+    firstNameForm: new FormControl('', [Validators.required]),
+    lastNameForm: new FormControl('', [Validators.required]),
+    emailForm: new FormControl('', [Validators.required, Validators.email]),
+    passwordForm: new FormControl(''),
+    passwordAgainForm: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit() {
+    //RECUPERATION DES DONNEES
     this.account = {firstName: "Jules", lastName: "Furret", email: "furretj@et.esiea.fr"};
-    //console.log(this.account.firstName);
+    //REMPLISSAGE DES FORMS
+    this.formGroup.controls.firstNameForm.setValue(this.account.firstName);
+    this.formGroup.controls.lastNameForm.setValue(this.account.lastName);
+    this.formGroup.controls.emailForm.setValue(this.account.email);
   }
 
   validate() {
+    //RECUPERATION DES FORMS
+    this.account.firstName = this.formGroup.controls.firstNameForm.value;
+    this.account.lastName = this.formGroup.controls.lastNameForm.value;
+    this.account.email = this.formGroup.controls.emailForm.value;
     console.log(this.account);
   }
 
