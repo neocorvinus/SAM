dfdea18b6c4c52c11a027c5ca6b81e6fd680c51c
TMachon <theo.machon.91@gmail.com>
2020-11-19 16:29:58 +0100

    API Google Maps init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Capacitor, Plugins, GeolocationPosition } from '@capacitor/core';
+import { Observable, of, from as fromPromise } from 'rxjs';
+import { tap, map, switchMap } from 'rxjs/operators';
+import { LoadingController, AlertController } from '@ionic/angular';
+
+const { Toast, Geolocation } = Capacitor.Plugins;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public coordinates: Observable<GeolocationPosition>;
+  public defaultPos: {
+    lattitude: 45,
+    longitude: 9
+  };
+
+  constructor(public loading: LoadingController, public alertCtrl: AlertController) {}
+
+  ngOnInit() { 
+    /**/
+    // start the loader
+    this.displayLoader()
+      .then((loader: any) => {
+        // get position
+        return this.getCurrentLocation()
+          .then(position => {
+            //close loader and return position
+            loader.dismiss();
+            return position;
+          })
+          // if error
+          .catch(err => {
+            // close loader and return Null
+            loader.dismiss();
+            return null;
+          });
+      });
+      /**
+      this.getCurrentLocation();
+      /**/
+  }
+
+  async displayLoader() {
+    const loading = await this.loading.create({
+      message: 'Loading...'
+    });
+    await loading.present();
+    return loading;
+  }
+
+  private async presentAlert(message: string): Promise<HTMLIonAlertElement> {
+    const alert = await this.alertCtrl.create({
+      header: "Alert!",
+      subHeader: "Application offline",
+      message: message,
+      buttons: ["OK"]
+    })
+    await alert.present();
+    return alert;
+  }
+
+  private async getCurrentLocation(): Promise<any> {
+    const isAvailable: boolean = Capacitor.isPluginAvailable("Geolocation");
+    if (!isAvailable) {
+      console.log("ERR: Plugin is not available");
+      return of (new Error("ERR: Plugin not available"));
+    }
+    const POSITION = Plugins.Geolocation.getCurrentPosition()
+    // handle Capacitor errors
+      .catch(err => {
+        console.log("ERR", err);
+        return new Error(err.message || "customized message");
+      });
+    this.coordinates = fromPromise(POSITION).pipe(
+      switchMap((data: any) => of(data.coords)),
+      tap(data => console.log(data))
+    );
+    return POSITION;
+  }
 
 }
